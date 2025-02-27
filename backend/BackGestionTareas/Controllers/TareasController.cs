f93756c564dc310839ce28c11686b307c2b768c5
CesarJIbanezV <cartod86@gmail.com>
2025-02-27 18:01:27 -0500

    Se modifica estructura para incluir test unitario

---
+++ backend/BackGestionTareas/Controllers/TareasController.cs
@@ -0,0 +1,59 @@
+﻿using BackGestionTareas.Context;
+using BackGestionTareas.Models;
+using BackGestionTareas.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackGestionTareas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TareasController : ControllerBase
+    {
+        private readonly ITareaService _tareaService;
+
+        public TareasController(ITareaService tareaService)
+        {
+            _tareaService = tareaService;
+        }
+        [HttpPost]
+        public async Task<ActionResult<TareaDto>> PostTarea([FromBody] TareaDto tarea)
+        {
+            await _tareaService.AddTarea(tarea);
+            return CreatedAtAction(nameof(GetTareaById), new { id = tarea.Id }, tarea);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<TareaDto>>> GetTareas()
+        {
+            return await _tareaService.GetAllTareas();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TareaDto>> GetTareaById(int id)
+        {
+            var tarea = await _tareaService.GetTareaById(id);
+            if (tarea == null) return NotFound();
+
+            return tarea;
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> UpdateTarea(int id, [FromBody] UpdateTareaDto tareaDto)
+        {
+            await _tareaService.UpdateTarea(id, tareaDto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTarea(int id)
+        {
+            await _tareaService.DeleteTarea(id);
+            return NoContent();
+        }        
+    }
+}
---
+++ backend/Test/TareasControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+using FluentAssertions;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+using BackGestionTareas.Controllers;
+using BackGestionTareas.Services;
+using System.Threading.Tasks;
+
+public class TareasControllerTests
+{
+    [Fact]
+    public async Task EliminarTarea_DeberiaRetornarNoContent_SiExisteAsync()
+    {
+        // Arrange
+        var mockService = new Mock<ITareaService>();
+        int tareaId = 1;
+        mockService.Setup(service => service.DeleteTarea(tareaId));
+
+        var controller = new TareasController(mockService.Object);
+
+        // Act
+        var resultado = await controller.DeleteTarea(tareaId);
+
+        // Assert
+        resultado.Should().BeOfType<NoContentResult>();
+    }
+}
